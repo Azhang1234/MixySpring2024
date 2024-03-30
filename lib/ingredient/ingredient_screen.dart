73f82ac9bf2b28ad41af3f553267b94b96e12c12
Lijun56 <zhu944@purdue.edu>
2024-03-30 17:51:53 -0400

    done

--- lib/ingredient/ingredient_screen.dart
+++ lib/ingredient/ingredient_screen.dart
@@ -8,7 +8,6 @@ import 'package:mixyspring2024/ui_view/mediterranean_diet_view.dart';
 import '../ui_view/title_view.dart';
 import 'meals_list_view.dart';
 
-//ingredient_screen.dart
 class IngredientScreen extends StatefulWidget {
   const IngredientScreen({Key? key, this.animationController})
       : super(key: key);
